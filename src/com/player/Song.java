55241368900e257d13e93b2499604d43dacc2625
mayxly <may.ly7303@gmail.com>
2022-04-28 22:32:56 -0400

    Create song class with constructors, getters and toString().

--- src/com/player/Song.java
+++ src/com/player/Song.java
@@ -1,4 +1,30 @@
 package com.player;
 
 public class Song {
+    String title;
+    String artist;
+    double duration;
+
+    public Song(String title, String artist,  double duration) {
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getArtist() {
+        return artist;
+    }
+    public double getDuration() {
+        return duration;
+    }
+    @Override
+    public String toString() {
+        return "Song { Title: " + title + "\"" +
+                        "Artist: " + artist + "\"" +
+                        "Duration: " + duration + " }";
+
+    }
 }
