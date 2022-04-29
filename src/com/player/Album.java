62da033fa74b425ad152ae01e458bee7cdf7ca7f
mayxly <may.ly7303@gmail.com>
2022-04-29 00:30:07 -0400

    Implement addToPlaylist() by track number and song title. Tested in main.

--- src/com/player/Album.java
+++ src/com/player/Album.java
@@ -1,6 +1,7 @@
 package com.player;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Album {
     private String name;
@@ -8,8 +9,9 @@ public class Album {
     private String artist;
     private ArrayList<Song> songs;
 
-    public Album(String name, String artist, ArrayList<Song> songs) {
+    public Album(String name, String artist) {
         this.name = name;
+        this.artist = artist;
         this.songs = new ArrayList<Song>();
     }
 
@@ -19,21 +21,47 @@ public class Album {
                 return songs.get(i);
             }
         }
-            return null;
+        return null;
     }
 
     public boolean addSong(String title, double duration) {
-        if(findSong(title) == null) {
+        if (findSong(title) == null) {
             songs.add(new Song(title, duration));
-            System.out.println(title + " was successfully added to the playlist.");
+            System.out.println(title + " was successfully added to the album.");
             return true;
         }
-        else {
-            System.out.println(title + " already exists in the list.");
+            System.out.println(title + " already exists in the album.");
             return false;
-        }
     }
 
+    //Adding to a playlist with album track number
+    public boolean addToPlaylist(int trackNumber, LinkedList<Song> Playlist) {
+        int index = trackNumber - 1;
+        if (index > 0 && index <= this.songs.size()) {
+            Playlist.add(this.songs.get(index));
+            System.out.println("The song with track number " + trackNumber +
+                    " was successfully added to the playlist.");
+            return true;
+        }
+        System.out.println("The song with track number " + trackNumber + " does not exist.");
+        return false;
+    }
 
-
+    //Adding to a playlist with song title
+    public Song addToPlaylist(String title, LinkedList<Song> Playlist) {
+        for (int i = 0; i < this.songs.size(); i++) {
+            if (this.songs.get(i).getTitle().equals(title)) {
+                Playlist.add(this.songs.get(i));
+                System.out.println(title + " was successfully added to the playlist.");
+                return this.songs.get(i);
+            }
+        }
+        System.out.println(title + " does not exist in the album.");
+        return null;
+    }
 }
+
+
+
+
+
--- src/com/player/Main.java
+++ src/com/player/Main.java
@@ -1,9 +1,34 @@
 package com.player;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
 
+    private static ArrayList<Album> albums = new ArrayList<>();
     public static void main (String [] args) {
-        System.out.println("Hello World");
+
+        Album album1 = new Album("Justice", "Justin Bieber");
+
+        album1.addSong("Holy", 3.5);
+        album1.addSong("Peaches", 4.2);
+        album1.addSong("Hold On", 4.1);
+        album1.addSong("Anyone", 3.8);
+
+        Album album2 = new Album("Immunity", "Clairo");
+
+        album2.addSong("Softly", 4.1);
+        album2.addSong("Bags", 4.5);
+        album2.addSong("Sofia", 3.9);
+
+        albums.add(album1);
+        albums.add(album2);
+
+        LinkedList<Song> playlist1 = new LinkedList<>();
+        albums.get(0).addToPlaylist("Holy", playlist1);
+        albums.get(0).addToPlaylist("Anyone", playlist1);
+        albums.get(1).addToPlaylist("Bags", playlist1);
+
     }
 
 }
